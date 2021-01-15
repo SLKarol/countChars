47670be13df96ddcd1d9359af1987ada79e8d7ed
Stanislav Karol <slsinitsin@gmail.com>
2021-01-15 12:16:58 +0300

    feature: библиотека countChars

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = countChars;
+
+/**
+ * Количество символов в строке
+ * Input: "abddfghgrab"
+ * Output: {a:2,b:2,d:2,f:1,g:2,h:1,r:1}
+ * @param {string} inputString Строка поиска
+ */
+function countChars(inputString) {
+  let re = {};
+  for (let i = 0; i < inputString.length; i++) {
+    let t = inputString[i];
+    if (Object.keys(re).find((q) => q === t)) {
+      re[t]++;
+    } else {
+      re[t] = 1;
+    }
+  }
+  return re;
+}
